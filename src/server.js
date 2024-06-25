647cf6592065117489d448c9d1dc7a55dd974d43
NaufalFikriHakim <naufalfikri2903@gmail.com>
2024-06-25 16:40:00 +0700

    implement hapi

--- src/server.js
+++ src/server.js
@@ -1,17 +1,13 @@
-const http = require('http');
-
-const requestListener = (request, response) => {
-    response.setHeader('Content-Type', 'text/html');
-    response.statusCode = 200;
-
-    response.end('<h1>Halo HTTP Server!</h1>');
+const Hapi = require('@hapi/hapi');
+     
+const init = async () => {
+  const server = Hapi.server({
+    port: 9000,
+    host: 'localhost',
+  });
+ 
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
 };
-
-const server = http.createServer(requestListener);
-
-const port = 9000;
-const host = 'localhost';
-
-server.listen(port, host, () => {
-    console.log(`Server berjalan pada http://${host}:${port}`);
-})
+ 
+init();
